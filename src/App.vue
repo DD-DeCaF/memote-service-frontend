fa1e99afab2648a4b53101894a48609094306d64
Ali Kaafarani <ali@kvikshaug.no>
2018-11-02 19:05:29 +0100

    feat: add tasks table

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <Header/>
-    <Upload/>
+    <Upload @task-created="addTask" />
+    <Tasks v-bind:tasks="tasks" />
     <Welcome/>
     <Footer/>
   </div>
 </template>
 
 <script>
+import * as axios from 'axios';
+
 import Header from './components/Header.vue';
 import Upload from './components/Upload.vue';
+import Tasks from './components/Tasks.vue';
 import Welcome from './components/Welcome.vue';
 import Footer from './components/Footer.vue';
 
@@ -18,9 +22,56 @@ export default {
   components: {
     Header,
     Upload,
+    Tasks,
     Welcome,
     Footer,
-  }
+  },
+  data: () => ({
+    tasks: []
+  }),
+  methods: {
+    readTasks() {
+      if(localStorage.getItem('tasks') === null) {
+        this.persistTasks();
+      } else {
+        this.tasks = JSON.parse(localStorage.getItem('tasks'));
+      }
+    },
+    addTask(uuid) {
+      this.tasks.push({
+        uuid: uuid,
+        submitted: new Date(),
+        status: 'PENDING',
+      });
+      this.persistTasks();
+    },
+    persistTasks() {
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    },
+    pollTasks() {
+      this.tasks.forEach((task) => {
+        if(task.status === 'SUCCESS' || task.status == 'FAILURE') {
+          return;
+        }
+
+        axios
+          .get(`http://localhost:8000/memote-webservice/status/${task.uuid}`)
+          .then(response => {
+            console.log(response.data);
+            task.status = response.data.status;
+          }).catch(error => {
+            // TODO
+            console.log(error);
+          });
+      });
+      this.persistTasks();
+      setTimeout(this.pollTasks, 1000);
+    },
+  },
+  created() {
+    this.readTasks();
+    this.pollTasks();
+  },
 };
 </script>
 
---
+++ src/components/Tasks.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <table class="striped" v-if="tasks.length > 0">
+      <thead>
+        <tr>
+          <th>UUID</th>
+          <th>Submitted</th>
+          <th>Status</th>
+          <th>View results</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="task in tasks">
+          <td>{{ task.uuid }}</td>
+          <td>{{ task.submitted }}</td>
+          <td>
+            <span v-if="task.status === 'PENDING'">In queue</span>
+            <span v-if="task.status === 'STARTED'">Started</span>
+            <span v-if="task.status === 'FAILURE'">Errored</span>
+            <span v-if="task.status === 'SUCCESS'">Completed</span>
+            <span v-if="task.status === 'EXPIRED'">Expired</span>
+          </td>
+          <td>
+            <div class="preloader-wrapper small active" v-if="task.status === 'PENDING'">
+              <div class="spinner-layer spinner-green-only">
+                <div class="circle-clipper left">
+                  <div class="circle"></div>
+                </div><div class="gap-patch">
+                  <div class="circle"></div>
+                </div><div class="circle-clipper right">
+                  <div class="circle"></div>
+                </div>
+              </div>
+            </div>
+            <div class="progress" v-if="task.status === 'STARTED'">
+              <!-- TODO: determinate progress -->
+              <div class="indeterminate"></div>
+            </div>
+            <p v-if="task.status === 'FAILED'">View error report (TODO)</p>
+            <a v-if="task.status === 'SUCCESS'" class="btn" :href="'report/' + task.uuid" target="_blank">
+              <i class="material-icons left">done</i>
+              View report
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tasks',
+  props: ['tasks'],
+}
+</script>
